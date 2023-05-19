a5a01111dda941e9441765734b88ad0b3af95495
Irina-Kr <133982815+Irina-Kr@users.noreply.github.com>
2023-05-19 23:19:44 +0300

    firsthomework

---
+++ src/JavaIntro.java
@@ -0,0 +1,21 @@
+//The first Java lesson
+
+public class JavaIntro
+{
+    public static void main(String[] args)
+    {
+        //Integrated output class
+        System.out.println("Welcome to Java Profession Lesson."); // ln -> next line
+
+        //print
+        System.out.print("Welcome to Java Profession Lesson with print"); //выведи мне сообщение и там же останься
+
+        //print
+        System.out.print("Welcome to Java.");
+        System.out.print("This is the next line...");
+
+        //println vs. print
+        System.out.println(); // любой вывод требует чего то на выход - знак следующей строчки
+        System.out.print(""); //error
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+// The first Java Lesson
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        //Integrated output class
+        System.out.println("Welcome to Java Profession Lesson.");
+    }
+}
