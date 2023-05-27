70435021e8842d21fb4b900d32834271c229d9bf
Irina-Kr <133982815+Irina-Kr@users.noreply.github.com>
2023-05-28 00:32:55 +0300

    fourthhomeworkwithloop

--- src/JavaIntro.java
+++ src/JavaIntro.java
@@ -1,9 +1,9 @@
 //The first Java lesson
 
-public class JavaIntro
-{
-    public static void main(String[] args)
-    {
+import java.util.Scanner;
+
+public class JavaIntro {
+    public static void main(String[] args) {
         //Integrated output class
         System.out.println("Welcome to Java Profession Lesson."); // ln -> next line
 
@@ -17,5 +17,56 @@ public class JavaIntro
         //println vs. print
         System.out.println(); // любой вывод требует чего то на выход - знак следующей строчки
         System.out.print(""); //error
+        // showOddNumbers();
+        // showReverseOrderOfNumbers();
+        // getSumOfNumbers();
+       // showTheSubsequenceOfNumbers();
+        showTheSubsequenceOfNegativeNumbers();
+    }
+
+    public static void showOddNumbers() {
+        for (int x = 1; x < 99; x++) {
+            System.out.println("for x:" + x++);
+            if (x % 2 != 0) {
+                System.out.print("\n");
+            }
+
+        }
+    }
+
+    public static void showReverseOrderOfNumbers() {
+        for (int x = 5; 1 <= x; x--) {
+            System.out.println("x:" + x);
+        }
+    }
+
+    public static void getSumOfNumbers() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number:");
+        int x = scanner.nextInt();
+        int sum = 0;
+        for (int y = 1; y <= x; y++) {
+            sum = sum + y;
+        }
+        System.out.print("sum:" + sum);
+    }
+
+    public static void showTheSubsequenceOfNumbers() {
+        int x = 7;
+        while (x <= 98) {
+            System.out.print(x + " ");
+            x = x + 7;
+        }
+    }
+
+    public static void showTheSubsequenceOfNegativeNumbers() {
+        int a = 5;
+        int b = 5;
+        while (-45 <= a) {
+            System.out.print(a - b);
+            a = a - b;
+
+        }
     }
 }
+
