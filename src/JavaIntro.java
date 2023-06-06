36f4efd7604d4c919b51ff1663a46600036470a3
Irina-Kr <133982815+Irina-Kr@users.noreply.github.com>
2023-06-06 16:32:14 +0300

    homework6

--- src/JavaIntro.java
+++ src/JavaIntro.java
@@ -1,9 +1,10 @@
 //The first Java lesson
 
-public class JavaIntro
-{
-    public static void main(String[] args)
-    {
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class JavaIntro {
+    public static void main(String[] args) {
         //Integrated output class
         System.out.println("Welcome to Java Profession Lesson."); // ln -> next line
 
@@ -17,5 +18,66 @@ public class JavaIntro
         //println vs. print
         System.out.println(); // любой вывод требует чего то на выход - знак следующей строчки
         System.out.print(""); //error
+         createThreeDimensionalArray();
+        paintTheChessBoard();
+    }
+
+    public static void createThreeDimensionalArray() {
+        int[][][] myArray = new int[][][]
+                {
+                        {
+                                {5, 1, 7}
+                        },
+                        {
+                                {15, 9, 11},
+                                {3, 6, 13}
+                        },
+                        {
+                                {4, 8, 25, 17, 3},
+                                {12, 0, 6, 24, 3},
+                                {13, 2, 0, 9, 4}
+                        },
+                        {
+                                {2, 1, 4, 7, 2, 9, 24}
+                        },
+                        {
+                                {3, 12, 7},
+                                {5, 2, 10}
+
+                        }
+                };
+        System.out.println(Arrays.deepToString(myArray));
+        //add a number to the array.
+        Scanner scanner = new Scanner(System.in);
+        int number;
+        System.out.print("Please,enter the number:");
+        number = scanner.nextInt();
+        for (int a = 0; a < myArray.length; a++) {
+            for (int b = 0; b < myArray[a].length; b++) {
+                for (int c = 0; c < myArray[a][b].length; c++) {
+                    myArray[a][b][c] += number;
+                }
+            }
+        }
+        System.out.println(Arrays.deepToString(myArray));
+    }
+
+    public static void paintTheChessBoard() {
+        String[][] chessBoard = new String[8][8];
+        for (int x = 0; x < chessBoard.length; x++) {
+            for (int y = 0; y < chessBoard.length; y++) {
+                if ((x + y) % 2 == 0) {
+                    chessBoard[x][y] = "W";
+                } else {
+                    chessBoard[x][y] = "B";
+                }
+            }
+        }
+        for (int x = 0; x < chessBoard.length; x++) {
+            for (int y = 0; y < chessBoard.length; y++) {
+                System.out.print(chessBoard[x][y] + " ");
+            }
+            System.out.println();
+        }
     }
 }
